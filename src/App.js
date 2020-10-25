f18f79f63f05b6976df7dac8ef92f71ac663529e
unknown <desarrolloweb@disdelsa.com>
2020-10-24 19:43:47 -0600

    cambios

--- src/App.js
+++ src/App.js
@@ -1,22 +1,111 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import io from "socket.io-client";
+import "./App.css";
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+import Box from "@material-ui/core/Box";
+const ENDPOINT = "http://localhost:4000/";
+
+
+const useStyles = makeStyles({
+  root: {
+    maxWidth: 345,
+  },
+  media: {
+    height: 140,
+  },
+});
 
 function App() {
+  const classes = useStyles();
+  
+  const [temp, settem] = useState("");
+  useEffect(() => {
+    const socket = io(ENDPOINT);
+    socket.on("temperatura", (data) => {
+      data.forEach((element) => {
+        settem(element);
+      });
+    });
+  }, [settem]);
+ console.log(temp)
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <Card className={classes.root}>
+          <CardActionArea>
+            <CardContent>
+              <Typography
+                style={{
+                  fontFamily: "Lobster",
+                  textAlign: "center",
+                  fontSize: "50px",
+                }}
+              >
+                <Box fontWeight="fontWeightBold">
+                  {temp.aire?"  Aire ": temp.co2?"   CO2  ":" Gas  "}
+                 
+                </Box>
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+          <CardActions>
+            <Button size="small" fullWidth color="primary">
+            Mq135 Aire  C02  Gas  
+            </Button>
+          </CardActions>
+        </Card>
+        &nbsp; &nbsp;
+    
+        <Card className={classes.root}>
+          <CardActionArea>
+            <CardContent>
+              <Typography
+                style={{
+                  fontFamily: "Lobster",
+                  textAlign: "center",
+                  fontSize: "50px",
+                }}
+              >
+                <Box fontWeight="fontWeightBold">
+                  {temp.temperatura + " °C "}
+                </Box>
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+          <CardActions>
+            <Button size="small" fullWidth color="primary">
+              temperatura
+            </Button>
+          </CardActions>
+        </Card>
+        &nbsp; &nbsp;
+        <Card className={classes.root}>
+          <CardActionArea>
+            <CardContent>
+              <Typography
+                style={{
+                  fontFamily: "Lobster",
+                  textAlign: "center",
+                  fontSize: "50px",
+                }}
+              >
+                <Box fontWeight="fontWeightBold">{temp.luminicidad<75?"Encendido":"Apagado"}</Box>
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+          <CardActions>
+            <Button size="small" fullWidth color="primary">
+              Luz
+            </Button>
+          </CardActions>
+        </Card>
+
       </header>
     </div>
   );
---
+++ src/server.js/index.js
@@ -0,0 +1,112 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const servidor = http.createServer(app);
+const socketio = require("socket.io");
+const io = socketio(servidor);
+const { Board, Sensor, Led } = require("johnny-five");
+const board = new Board();
+//modelo de la base de datos
+const modelTemp = require("./ArduinoModel");
+///base de datos
+const conectarDB = require("./db");
+conectarDB();
+//
+app.use(express.static(__dirname+'/../../build'));
+//Sensores
+let MQ135, LM35, LDR;
+//Led a Encender
+let celsuistemp, ledAire, ledC02, ledGas, led11, led12, led13;
+let valueM0="",valueM1="",valueM2="";
+
+board.on("ready", () => {
+  const Sensor_Aire_CO2_Gas = { pin: "A0", freq: 50 };
+  const SendorTemperatura = { pin: "A1", freq: 50 };
+  const SendorLuz = { pin: "A2", freq: 50 };
+
+  MQ135 = new Sensor(Sensor_Aire_CO2_Gas);
+  LM35 = new Sensor(SendorTemperatura);
+  LDR = new Sensor(SendorLuz);
+
+  ondear();
+});
+
+const ondear = async () => {
+  const valorMq135 = MQ135.value;
+  const valorTemperatura = LM35.value;
+  const valorLuz = LDR.value;
+  ledAire = new Led(8);
+  ledC02 = new Led(9);
+  ledGas = new Led(10);
+  led11 = new Led(11);
+  led12 = new Led(12);
+  led13 = new Led(13);
+ console.log(valorMq135)
+  //MQ135//
+  if (valorMq135 <= 55) {
+    ledAire.on();
+    ledC02.off();
+    ledGas.off();
+    valueM0=valorMq135;
+  }  if (valorMq135 >= 56 && valorMq135 <= 65) {
+    ledAire.off();
+    ledC02.on();
+    ledGas.off();
+    valueM1=valorMq135;
+  }  if (valorMq135 >= 74 && valorMq135 <= 350) {
+    ledAire.off();
+    ledC02.off();
+    ledGas.on();
+    valueM2=valorMq135;
+  }
+
+  //Temperatura//
+  celsuistemp = (5 * valorTemperatura * 100) / 1024;
+  if (celsuistemp > 22) {
+    led13.on();
+    led12.off();
+  } else {
+    led12.on();
+    led13.off();
+  }
+
+  //Luz//
+  if (valorLuz < 75) {
+    led11.on();
+  } else {
+    led11.off();
+  }
+  try {
+    let body = {
+      temperatura: parseInt(celsuistemp),
+      luminicidad: valorLuz,
+      aire:valueM0,
+      co2:valueM1,
+      gas:valueM2
+    };
+
+    // console.log(body)
+    // User=new modelTemp(body);
+    //   await User.save()
+
+    const ver = await modelTemp.find({}).sort({ _id: -1 }).limit(1);
+
+     console.log(ver);
+    
+     io.sockets.on('connection', function (socket) {
+
+      socket.emit("temperatura", ver);
+     })
+     
+   // io.emit("temperatura", ver);
+  } catch (error) {
+    console.log(error);
+  }
+
+  setTimeout(ondear, 1000);
+};
+
+const port = process.env.PORT || 4000;
+servidor.listen(port,'0.0.0.0', () => {
+  console.log(`el puerto esta funcionando  ${port}`);
+});
